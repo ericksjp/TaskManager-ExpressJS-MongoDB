7e5362f0d261927f490e0d23c6775d2e5dbda9c7
Erick <ericksjp703@gmail.com>
2024-02-10 16:59:54 -0300

    Configurando arquivo .env e se conectando ao banco de dados

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,28 @@
-import express from 'express';
-import routes from './routes/index.mjs';
+import { connectDB } from "./db/connect.mjs";
+import express from "express";
+import routes from "./routes/index.mjs";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send({"msg":"primeiro passo!!!"});
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "primeiro passo!!!" });
 });
-
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
