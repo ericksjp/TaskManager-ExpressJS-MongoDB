e0cf2e5d5cd8c639db28c9f93fc49c04e12c5099
Erick <ericksjp703@gmail.com>
2024-02-10 18:28:16 -0300

    Implementando o CRUD das tasks

--- src/controllers/tasks.mjs
+++ src/controllers/tasks.mjs
@@ -1,21 +1,58 @@
-const getAllTasks = (req, res) => {
-  res.send('Get all tasks');
+import { Task } from "../models/task.mjs";
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send
+  }
 }
 
-const createTask = (req, res) => {
-  res.send('Create task');
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    task.save();
+    res.status(200).send(req.body);
+  } catch (error) {
+    res.status(500).send({ meg: error});
+  }
 }
 
-const getTask = (req, res) => {
-  res.send('Get task');
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task)
+      return res.status(404).send({ msg: `Task with id ${taskID} not found`});
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send({ meg: error});
+  }
 }
 
-const updateTask = (req, res) => {
-  res.send('Update task');
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, { new: true, runValidators: true });
+    if (!task)
+      return res.status(404).send({ msg: `Task with id ${taskID} not found`});
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send({ meg: error});
+  }
 }
 
-const deleteTask = (req, res) => {
-  res.send('Delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task)
+      return res.status(404).send({ msg: `Task with id ${taskID} not found`});
+    res.status(200).send({ msg: `Task with id ${taskID} deleted`});
+  }catch (error) {
+    res.status(500).send({ meg: error});
+  }
 }
 
 export { getAllTasks, createTask, getTask, updateTask, deleteTask};
---
+++ src/models/task.mjs
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+    maxlength: [20, "Name can not be more than 20 characters"],
+
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export { Task };
